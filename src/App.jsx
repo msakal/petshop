05a2e0a680abd4f2c175ab9e18b20ae30b81d9d7
MarcelloSA <103936996+msakal@users.noreply.github.com>
2022-10-19 11:44:24 -0300

    Componentes: Cabeçalho e Menu
    Revisão e aplicação de CSS global e com módulos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+/* Sempre colocar o caminho exato da chamada.. './pasta/componente(s)' */
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
 const App = () => {
-    return <h1>PetShop</h1>;
+  return (
+    <>
+      <Cabecalho />
+    </>
+  );
 };
 
 export default App;
